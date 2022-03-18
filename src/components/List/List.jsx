b7555c44a99db0f5d8d3776e5df162765d7b5abb
B0AY-C0D3GR0UND <99433641+B0AY-C0D3GR0UND@users.noreply.github.com>
2022-03-18 23:07:44 +0500

    get places data from and setting up the locations

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,12 +1,23 @@
 import React,{useState} from 'react'
 import { CircularProgress, Grid,Typography, inputLabel , MenuItem, FormControl , Select, InputLabel, Menu } from '@material-ui/core';
 
+import PlaceDetails from '../PlaceDetails/PlaceDetails';
 import useStyles from './styles';
 const List = () => {
   const classes = useStyles();
   const [type , setType] = useState('resturants');
   const [rating , setRating] = useState('');
 
+  const places = [
+    {name:'Cool Place'},
+    {name:'best Place'},
+    {name:'awesome Place'},
+    {name:'my Place'},
+    {name:'Cool Place'},
+    {name:'best Place'},
+    {name:'awesome Place'},
+    {name:'my Place'},
+];
   return (
     <div className={classes.container}>
         <Typography variant="h4">Resturants , Hotels & Attractions around you</Typography>
@@ -28,6 +39,13 @@ const List = () => {
               <MenuItem value = {4.5}>Above 4.5</MenuItem>
           </Select>
         </FormControl>
+        <Grid container spacing={3} className = {classes.list}>
+          {places?.map((place , i)=>(
+            <Grid item key={i} xs={12}>
+              <PlaceDetails place={place}/>
+            </Grid>
+          ))}
+        </Grid>
     </div>
   )
 }
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -6,24 +6,27 @@ import Rating from '@material-ui/lab';
 
 import useStyles from './styles';
 
-const Map = () => {
+const Map = ({coordinates,setCoordinates,setBounds}) => {
+  
   const classes = useStyles();
   const isMobile = useMediaQuery('(min-width :600px)');
-
-  const coordinates = { lat: 0 , lng: 0}; 
   return (
     <div className={classes.mapContainer}>
-      <GoogleMapReact bootstrapURLKeys={{key:'AIzaSyBFMaoJSlx_6KgoTh_PHbCz0ursSz3vyzI'}}
+      <GoogleMapReact 
+      bootstrapURLKeys={{key:'AIzaSyBFMaoJSlx_6KgoTh_PHbCz0ursSz3vyzI'}}
       defaultCenter={coordinates}
       center={coordinates}
       defaultZoom={14}
       margin={[50,50,50,50]}
       options={''}
-      onChange={''}
+      onChange={(e)=>{
+        setCoordinates({lat: e.center.lat , lng:e.center.lng})
+        setBounds({ne:e.marginBounds.ne , sw:e.marginBounds.sw})
+      }}
       onChildClick={''}
       >
       </GoogleMapReact>
-
+ 
     </div>
   )
 }
