b36ce2fa2b3cc41ec33c9a469f480b53c23bd840
B0AY-C0D3GR0UND <99433641+B0AY-C0D3GR0UND@users.noreply.github.com>
2022-03-20 17:41:18 +0500

    finishing up the things

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,60 +1,60 @@
-import React from 'react'
+import React from 'react';
 import GoogleMapReact from 'google-map-react';
-import { Paper,Typography ,useMediaQuery  } from '@material-ui/core';
-import LocationOnOutlinedIcon  from '@material-ui/icons/LocationOnOutlined';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
 import Rating from '@material-ui/lab/Rating';
 
-import useStyles from './styles';
+import mapStyles from '../../mapStyles';
+import useStyles from './styles.js';
 
-const Map = ({coordinates,setCoordinates,setBounds,places}) => {
-  
+const Map = ({ coords, places, setCoords, setBounds, setChildClicked, weatherData }) => {
+  const matches = useMediaQuery('(min-width:600px)');
   const classes = useStyles();
-  const isDesktop = useMediaQuery('(min-width :600px)');
+
   return (
     <div className={classes.mapContainer}>
-      <GoogleMapReact 
-      bootstrapURLKeys={{key:'AIzaSyBFMaoJSlx_6KgoTh_PHbCz0ursSz3vyzI'}}
-      defaultCenter={coordinates}
-      center={coordinates}
-      defaultZoom={14}
-      margin={[50,50,50,50]}
-      options={''}
-      onChange={(e)=>{
-        setCoordinates({lat: e.center.lat , lng:e.center.lng})
-        setBounds({ne:e.marginBounds.ne , sw:e.marginBounds.sw})
-      }}
-      onChildClick={''}
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
+        defaultCenter={coords}
+        center={coords}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={{ disableDefaultUI: true, zoomControl: true, styles: mapStyles }}
+        onChange={(e) => {
+          setCoords({ lat: e.center.lat, lng: e.center.lng });
+          setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
+        }}
+        onChildClick={(child) => setChildClicked(child)}
       >
-        {places?.map((place,i)=>(
-            <div
+        {places.length && places.map((place, i) => (
+          <div
             className={classes.markerContainer}
             lat={Number(place.latitude)}
-            lng = {Number(place.longitude)}
+            lng={Number(place.longitude)}
             key={i}
-            >
-            {
-              !isDesktop?(
-                <LocationOnOutlinedIcon color="primary" fontSize='large'/>
-              ):(
+          >
+            {!matches
+              ? <LocationOnOutlinedIcon color="primary" fontSize="large" />
+              : (
                 <Paper elevation={3} className={classes.paper}>
-                  <Typography className={classes.Typography} variant = "subtitle2" gutterBottom>
-                  {place.name}
-                  </Typography>
-                  <img 
+                  <Typography className={classes.typography} variant="subtitle2" gutterBottom> {place.name}</Typography>
+                  <img
                     className={classes.pointer}
-                    src={place.photo ? place.photo.images.large.url : 'https://media-cdn.tripadvisor.com/media/photo-s/1a/b8/46/6d/london-stock.jpg'}
-                  alt={place.name}
+                    src={place.photo ? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
                   />
-                  <Rating size="small" value={Number(place.rating)} readOnly/>
+                  <Rating name="read-only" size="small" value={Number(place.rating)} readOnly />
                 </Paper>
-              )
-            }
-            </div>
+              )}
+          </div>
+        ))}
+        {weatherData?.list?.length && weatherData.list.map((data, i) => (
+          <div key={i} lat={data.coord.lat} lng={data.coord.lon}>
+            <img src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`} height="70px" />
+          </div>
         ))}
       </GoogleMapReact>
- 
     </div>
-  )
-}
+  );
+};
 
 export default Map;
--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,65 +1,70 @@
-import React from 'react'
-import { Box, Typography, Button, Card, CardMedia, CardContent, CardActions, Chip } from '@material-ui/core'
+import React from 'react';
+import { Box, Typography, Button, Card, CardMedia, CardContent, CardActions, Chip } from '@material-ui/core';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
 import PhoneIcon from '@material-ui/icons/Phone';
 import Rating from '@material-ui/lab/Rating';
 
-import useStyles from './styles';
-const PlaceDetails = ({ place }) => {
+import useStyles from './styles.js';
+
+const PlaceDetails = ({ place, selected, refProp }) => {
+  if (selected) refProp?.current?.scrollIntoView({ behavior: 'smooth', block: 'start' });
   const classes = useStyles();
+
   return (
     <Card elevation={6}>
       <CardMedia
         style={{ height: 350 }}
-        image={place.photo ? place.photo.images.large.url : 'https://media-cdn.tripadvisor.com/media/photo-s/1a/b8/46/6d/london-stock.jpg'}
+        image={place.photo ? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
         title={place.name}
       />
       <CardContent>
-        <Typography gutterBottom variant="h5 ">{place.name}</Typography>
-        <Box display="flex" justifycontent="space-between" >
-        <Rating value={Number(place.rating)} readOnly/>
-          <Typography gutterBottom variant="subtitle1">{place.price_level}</Typography>
+        <Typography gutterBottom variant="h5">{place.name}</Typography>
+        <Box display="flex" justifyContent="space-between" my={2}>
+          <Rating name="read-only" value={Number(place.rating)} readOnly />
+          <Typography component="legend">{place.num_reviews} review{place.num_reviews > 1 && 's'}</Typography>
         </Box>
-        <Box display="flex" justifycontent="space-between" >
-          <Typography variant="subtitle1">Price</Typography>
-          <Typography gutterBottom variant="subtitle1">Out of {place.num_reviews} Reviews</Typography>
+        <Box display="flex" justifyContent="space-between">
+          <Typography component="legend">Price</Typography>
+          <Typography gutterBottom variant="subtitle1">
+            {place.price_level}
+          </Typography>
         </Box>
-        <Box display="flex" justifycontent="space-between" >
-          <Typography variant="subtitle1">Ranking</Typography>
-          <Typography gutterBottom variant="subtitle1">{place.ranking}</Typography>
+        <Box display="flex" justifyContent="space-between">
+          <Typography component="legend">Ranking</Typography>
+          <Typography gutterBottom variant="subtitle1">
+            {place.ranking}
+          </Typography>
         </Box>
         {place?.awards?.map((award) => (
-          <Box my={1} display="flex" justifyContent="space-between" alignItems="center">
-            <img src={award.images.small} alt={award.display_name}></img>
+          <Box display="flex" justifyContent="space-between" my={1} alignItems="center">
+            <img src={award.images.small} />
             <Typography variant="subtitle2" color="textSecondary">{award.display_name}</Typography>
           </Box>
         ))}
         {place?.cuisine?.map(({ name }) => (
-          <Chip key={name} size="small" label={name} className={classes.chip}></Chip>
+          <Chip key={name} size="small" label={name} className={classes.chip} />
         ))}
-        {/* {place?.address?.map(({address})=>(
-          <Typography gutterBottom variant = "body2" color = "textSecondary" className ={classes.subtitle}>
-            <LocationOnIcon/> {address}
+        {place.address && (
+          <Typography gutterBottom variant="body2" color="textSecondary" className={classes.subtitle}>
+            <LocationOnIcon />{place.address}
           </Typography>
-      ))} */}
-
-        {<Typography gutterBottom variant="body2" color="textSecondary" className={classes.subtitle}>
-          <LocationOnIcon /> {place.address}
-        </Typography>}
-        {<Typography gutterBottom variant="body2" color="textSecondary" className={classes.spacing}>
-          <PhoneIcon /> {place.phone ? place.phone : "Null"}
-        </Typography>}
-        <CardActions>
-          <Button size="small" color="primary" onClick={() => window.open(place.web_url, '_blank')} >
-            Trip Advisor
-          </Button>
-          <Button size="small" color="primary" onClick={() => window.open(place.website, '_blank')} >
-            Website
-          </Button>
-        </CardActions>
+        )}
+        {place.phone && (
+          <Typography variant="body2" color="textSecondary" className={classes.spacing}>
+            <PhoneIcon /> {place.phone}
+          </Typography>
+        )}
       </CardContent>
+      <CardActions>
+        <Button size="small" color="primary" onClick={() => window.open(place.web_url, '_blank')}>
+          Trip Advisor
+        </Button>
+        <Button size="small" color="primary" onClick={() => window.open(place.website, '_blank')}>
+          Website
+        </Button>
+      </CardActions>
     </Card>
-  )
-}
+  );
+};
 
-export default PlaceDetails
+export default PlaceDetails;
